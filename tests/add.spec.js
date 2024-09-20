05607611d03b0cae465c60172a03eb0e89a4d896
monica am <monica.andres.consulting@gmail.com>
2024-09-20 17:58:40 +0200

    20.09

--- tests/add.spec.js
+++ tests/add.spec.js
@@ -19,6 +19,15 @@ describe("Iteration 1 | Getting Started", () => {
         expect(add()).toEqual(undefined);
         expect(add(undefined, 1)).toEqual(undefined);
       });
+
+      it('should return undefined is any of the arguments is not a number', () => {
+        expect(add(1)).toBeUndefined();
+        expect(add('a')).toBeUndefined();
+        expect(add(null)).toBeUndefined();
+        expect(add(1)).toBeUndefined();
+
+      })
+        
     });
   });
   
